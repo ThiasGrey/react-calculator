55d5e7b3ce2fc990ab0fee951303a2d2ac861dd3
bmgrey <bm.grey@outlook.com>
2021-01-06 16:26:01 -0300

    Ajustes Display e Botão

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1>Calculadora</h1>
+    <React.StrictMode>
+      <Calculator />
+    </React.StrictMode>
+  </div>
+  ,
   document.getElementById('root')
 );
 
